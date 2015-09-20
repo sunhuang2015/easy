78aa7087923ec0bc53f75410d072722b621f6ea8
sunhuang2015 <1469261019@qq.com>
2015-09-20 19:18:04 +0800

    Add Company and Depart

---
+++ database/migrations/2015_09_20_111040_create_departments_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDepartmentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('departments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('company_id')->unsigned()->index();
+            $table->string('name')->index();
+            $table->string('alias')->nullable();
+            $table->string('costcenter');
+            $table->foreign('company_id')->references('id')->on('companies')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('departments');
+    }
+}
